e21c763f2b89a1de687cc9c4a36d62f22b166738
Eithan ASSOULINE <eassouli@e2r9p10.42.fr>
2019-10-16 13:15:25 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,69 +6,108 @@
 /*   By: eassouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 16:22:59 by eassouli          #+#    #+#             */
-/*   Updated: 2019/10/15 17:47:45 by eassouli         ###   ########.fr       */
+/*   Updated: 2019/10/16 13:14:18 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char	**ft_leak(char **strsplit, size_t tab)
+{
+	while (tab >= 0)
+	{
+		free(strsplit[tab]);
+		tab--;
+	}
+	free(strsplit);
+	return (NULL);
+}
 
+char	**ft_alloc_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	b;
+	size_t	w;
+	char	**strsplit;
 
+	i = 0;
+	b = 0;
+	w = 1;
+	while (s[i])
+	{
+		if (s[i] == c)
+			b = 0;
+		else if (s[i] != c && b == 0)
+		{
+			w++;
+			b = 1;
+		}
+		i++;
+	}
+	if (!(strsplit = malloc(sizeof(char *) * w)))
+		return (0);
+	return (strsplit);
+}
 
+size_t	ft_word_size(char const *s, char c, size_t i)
+{
+	size_t	size;
 
+	size = 1;
+	while (s[i] && s[i] != c)
+	{
+		i++;
+		size++;
+	}
+	return (size);
+}
+
+char	*ft_tab_split(char const *s, char c, size_t i)
+{
+	size_t	j;
+	char	*strsplit;
+
+	j = 0;
+	if (!(strsplit = malloc(sizeof(char) * ft_word_size(s, c, i))))
+		return (0);
+	while (s[i] && s[i] != c)
+	{
+		strsplit = s[i];
+		i++;
+		j++;
+	}
+	strsplit = '\0';
+	return (strsplit);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
 	size_t	b;
-	char **split;
+	size_t	tab;
+	char	**strsplit;
 
 	i = 0;
-	j = 1;
-	b = 0;
+	tab = 0;
+	if (!s)
+		return (0);
+	if ((strsplit = ft_alloc_split(s, c)) == 0)
+		return (0);
+	b = 1;
 	while (s[i])
 	{
-		if (s[i] == c && b == 1)
+		if (s[i] != c && b == 1)
 		{
-			j++;
+			if ((strsplit[tab] = ft_tab_split(s, c, i) == 0))
+				return (ft_leak(strsplit, tab));
 			b = 0;
+			tab++;
 		}
-		if (s[i] != c)
+		else if (s[i] == c)
 			b = 1;
 		i++;
 	}
-	if (!(split = malloc(sizeof(char *) * j)))
-		return (0);
-	i = 0;
-	b = 0;
-	while (s[i])
-	{
-		while (s[i] && s[i] == c)
-			i++;
-		j = 0;
-		k = i;
-		while (s[k] && s[k] != c)
-		{
-			k++;
-			j++;
-		}
-		if (j > 0)
-		{
-			if (!(split[b] = malloc(sizeof(char) * (j + 1))))
-				return (0);
-			j = 0;
-			while (s[i] && s[i] != c)
-			{
-				split[b][j] = s[i];
-				i++;
-				j++;
-			}
-			split[b][j] = '\0';
-			b++;
-		}
-	}
-	split[b] = 0;
-	return (split);
+	strsplit[tab] = 0;
+	return (strsplit);
 }
