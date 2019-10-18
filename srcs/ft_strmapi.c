8ced1541fc2aee5dd1b09464bead8965a374edd5
Eithan ASSOULINE <eassouli@e3r11p2.42.fr>
2019-10-18 13:47:51 +0200

    move in srcs

--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -5,9 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eassouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/15 17:00:56 by eassouli          #+#    #+#             */
-/*   Updated: 2019/10/15 17:01:04 by eassouli         ###   ########.fr       */
+/*   Created: 2019/10/18 13:31:13 by eassouli          #+#    #+#             */
+/*   Updated: 2019/10/18 13:39:44 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s)
+		return (0);
+	if (!(str = malloc(sizeof(char) * ft_strlen(s))))
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
