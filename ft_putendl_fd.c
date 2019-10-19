1e140c6d1194f1ee11abd180e3a3f3b03ecbec71
eithan assouline <eithan@MacBook-Pro-de-eithan.local>
2019-10-19 21:32:31 +0200

    all fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -11,3 +11,9 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -11,3 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s));
+}
