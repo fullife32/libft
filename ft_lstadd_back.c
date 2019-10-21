4aca79a7c8aac00506bc4289639142280d4f590b
Eithan ASSOULINE <eassouli@e2r9p8.42.fr>
2019-10-21 11:07:23 +0200

    lstaddone, delone

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eassouli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/21 10:45:53 by eassouli          #+#    #+#             */
+/*   Updated: 2019/10/21 10:47:50 by eassouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_lstadd_back(t_list **alst, t_list *new)
@@ -9,6 +21,6 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 		ft_lstadd_front(alst, new);
 		return ;
 	}
-    lst = ft_lstlast(*alst);
+	lst = ft_lstlast(*alst);
 	lst->next = new;
 }
