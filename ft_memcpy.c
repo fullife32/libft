c6cc41c2f9f4d423342c81c3747e63caec0933b5
Eithan ASSOULINE <eassouli@e2r6p3.42.fr>
2019-10-27 13:53:03 +0100

    redo

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: eassouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:44:15 by eassouli          #+#    #+#             */
-/*   Updated: 2019/10/26 12:00:18 by eassouli         ###   ########.fr       */
+/*   Updated: 2019/10/27 12:11:05 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t			i;
 	unsigned char	*dest;
 	unsigned char	*source;
+	size_t			i;
 
 	if (!dst && !src && n > 0)
 		return (NULL);
